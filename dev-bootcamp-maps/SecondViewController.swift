ee9bb5a4fa1b73c4e2c662c3c16d07cd311fe6a4
Bartosz Odrzywolek <bartosz.odrzywolek@gmail.com>
2016-05-15 13:08:41 +0200

    Initial Commit

---
+++ dev-bootcamp-maps/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  dev-bootcamp-maps
+//
+//  Created by Bartosz Odrzywołek on 15/05/16.
+//  Copyright © 2016 Bartosz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
